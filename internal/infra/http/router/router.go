cf42e2b3c90a577a06a625d11203d2f6825ee58d
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-09-02 20:08:51 -0300

    refactor: move routing initializer to it's own file

---
+++ internal/infra/http/router/router.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ranielsobrinho/scheduling-service-api/internal/data/usecases"
+	db "github.com/ranielsobrinho/scheduling-service-api/internal/infra/database/helpers"
+	"github.com/ranielsobrinho/scheduling-service-api/internal/infra/database/repositories"
+	"github.com/ranielsobrinho/scheduling-service-api/internal/presentation/controllers"
+)
+
+func Initialize() {
+	server := gin.Default()
+	dbConnection, err := db.ConnectDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Repository
+	SchedulingRepository := repositories.NewSchedulingRepository(dbConnection)
+
+	// UseCase
+	SchedulingUseCase := usecases.NewGetSchedulesUseCase(SchedulingRepository)
+
+	// Controller
+	SchedulingController := controllers.NewGetSchedulesController(SchedulingUseCase)
+
+	server.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+		})
+	})
+
+	server.GET("/api/schedules", SchedulingController.GetSchedules)
+
+	server.Run(":5050")
+}
