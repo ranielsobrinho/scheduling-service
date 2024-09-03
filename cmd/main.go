e40f66bfa8d239ef120ff4ac1786bd255595b977
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-09-03 10:58:39 -0300

    refactor: should load godotenv on init

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/ranielsobrinho/scheduling-service-api/internal/infra/http/router"
+import (
+	"github.com/joho/godotenv"
+	"github.com/ranielsobrinho/scheduling-service-api/internal/infra/http/router"
+)
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic(".env file couldn't be loaded")
+	}
+
 	// Init Router
 	router.Initialize()
 }
