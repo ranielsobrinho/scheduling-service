66beb55fb33133c5dc10bf4a0afc3af924fcd0be
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-09-05 00:57:33 -0300

    feat: add createSchedulingController

---
+++ internal/presentation/controllers/createSchedulingController.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ranielsobrinho/scheduling-service-api/internal/data/usecases"
+	"github.com/ranielsobrinho/scheduling-service-api/internal/domain/models"
+)
+
+type CreateSchedulingController struct {
+	createSchedulingUseCase usecases.CreateScheduleUseCase
+}
+
+func NewCreateSchedulingController(createSchedulingUseCase usecases.CreateScheduleUseCase) CreateSchedulingController {
+	return CreateSchedulingController{createSchedulingUseCase: createSchedulingUseCase}
+}
+
+func (createSchedulingController *CreateSchedulingController) CreateSchedule(ctx *gin.Context) {
+	var schedulingModel models.SchedulingModel
+
+	err := ctx.BindJSON(&schedulingModel)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedScheduling, err := createSchedulingController.createSchedulingUseCase.CreateScheduleUseCase(schedulingModel)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedScheduling)
+}
