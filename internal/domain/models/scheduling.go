69e3d52a96e9e932ab1384f74c3ec9c5ec1f9080
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-09-10 19:41:20 -0300

    refactor: change type of user_id and add create_at column

--- internal/domain/models/scheduling.go
+++ internal/domain/models/scheduling.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type SchedulingModel struct {
 	Id           uuid.UUID `json:"id"`
 	ScheduleDate string    `json:"schedule_date"`
-	User         string    `json:"user_id"`
+	User         int       `json:"user_id"`
 	Service      string    `json:"service"`
+	CreatedAt    string    `json:"created_at"`
 }
